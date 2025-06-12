65b0fbfe8ef0268037fd40fc5d4d4c635ce120f3
Imre Balogh <60578855+Iminet72@users.noreply.github.com>
2025-06-12 16:46:21 +0200

    Update __init__.py

--- custom_components/adap1/__init__.py
+++ custom_components/adap1/__init__.py
@@ -31,9 +31,9 @@ async def async_setup_entry(hass: HomeAssistant, config_entry):
 
     hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
-    # hass.async_create_task(
-    await hass.config_entries.async_forward_entry_setups(config_entry, "sensor")
-    # )
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
 
     return True
 
